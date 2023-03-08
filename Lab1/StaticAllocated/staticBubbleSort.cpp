c74fb60cedb4e6cf25a3d992af023ddb7bcc1a2c
Paulo Filipe <filipemsilva@outlook.com.br>
2023-03-08 10:14:00 -0300

    Moved file to correct directory

--- Lab1/StaticAllocated/staticBubbleSort.cpp
+++ Lab1/StaticAllocated/staticBubbleSort.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <chrono>
+
 using namespace std;
 
-
+void stressMode(char);
 void bubbleSort(int [], int);
 void fasterBubbleSort(int [], int);
 void displayArray(int [], int);
@@ -10,17 +12,35 @@ void recursiveFasterBubbleSort(int [], int);
 
 
 int main(int argc, char *argv[]){
+
+    // Stress mode test
+    if (argc == 3 && string(argv[2]) == "-stress"){
+        if ((argv[1][0] != 'f' && argv[1][0] != 'r' && argv[1][0] != 'n')){
+            cout << argv[1][0] << " is an not option" << endl;
+            return 0;
+        }
+        stressMode(argv[1][0]);
+        return 0;
+    }
+    
+
     // Ensure that the user has entered a VALID command line argument
     if (argc != 2 || (argv[1][0] != 'f' && argv[1][0] != 'r' && argv[1][0] != 'n')){
         cout << "Usage: sort f|n|r" << endl;
         cout << "f = faster bubble sort" << endl;
         cout << "n = normal bubble sort" << endl;
         cout << "r = recursive faster bubble sort" << endl;
+        cout << "f|n|r -stress = sort with stress mode" << endl;
+
         return 1;
     }
 
+
+
     int values[] = { 5, 7, 2, 8, 9, 1, 4, 3, 6 };
     int size = sizeof(values) / sizeof(int);
+
+    generateRandomArray(values, size);
 
     cout << "The unsorted values are " << endl;
 
@@ -141,6 +161,56 @@ void recursiveFasterBubbleSort(int array[], int size){
 
     // sort the remaining array
     recursiveFasterBubbleSort(array, newSize);
+}
+
+void stressMode(char option){
+    // Generate 1000 random vectors, each with a random number of random elements
+    // Print the average time it took to sort all the vectors
+
+    auto startAll = chrono::high_resolution_clock::now();
+    auto averageTime = 0;
+
+    cout << "Number of elements,Time taken by the sorting function" << endl;
+
+    // Generate 1000 random lists
+    for (int i = 0; i < 1000; i++){
+        int howManyNumbers = rand() % 1000 + 1;
+        int values[howManyNumbers];
+        generateRandomArray(values, howManyNumbers);
+
+        // Generate a random number of elements
+
+        // Sort the vector
+        auto start = chrono::high_resolution_clock::now();
+        switch (option)
+        {
+        case 'f':
+            fasterBubbleSort(values, howManyNumbers);
+            break;
+        case 'r':
+            recursiveFasterBubbleSort(values, howManyNumbers);
+            break;
+        case 'n':
+            bubbleSort(values, howManyNumbers);
+            break;
+        }
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+        
+        averageTime += duration.count();
+
+        // Print the number of elements of the vector and the time it took to sort it
+        cout << howManyNumbers << "," << duration.count() << endl;
+
+    }
+    auto endAll = chrono::high_resolution_clock::now();
+
+    // Average time: 0.0000000
+    cout << endl << "Average time: " << averageTime / 1000 << " microseconds" << endl;
+
+    // Total time: 0.0000000
+    auto durationAll = chrono::duration_cast<chrono::microseconds>(endAll - startAll);
+    cout << "Total time  : " << durationAll.count() << " microseconds" << endl << endl;
 }
 
 
